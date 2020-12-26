08ed9fdb3bdd64b4f0ee9ee6117b7c54041f3ce3
Vladyslav <kytsevolov@gmail.com>
2020-12-26 15:00:12 -0700

    displayed resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { API, graphqlOperation} from 'aws-amplify';
+
+// import  query defenitions
+import {listResources as ListResources} from './graphql/queries';
+
+
+interface IResources {
+  id: number
+  name: string
+  author: string
+  url: string
+}
 
 function App() {
+  const [listResources, setListResources] = useState<IResources[] | []>([]);
+
+  useEffect(() => {
+    getResources();
+  },[])
+
+  const getResources = async () => {
+    try {
+      const resourcesData: any = await API.graphql(graphqlOperation(ListResources));
+      setListResources(resourcesData.data.listResources.items);
+
+    } catch (error) {
+      console.log(' get resources error', error);
+    }
+  }
+
+  console.log('listResources',listResources)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(listResources as IResources[]).map((resource: IResources, i: number) => {
+        return (
+        <div key={i}>
+            <h2>{resource.name}</h2>
+            <p>{resource.author}</p>
+            <p>{resource.url}</p>
+        </div>
+      )})}
     </div>
   );
 }
